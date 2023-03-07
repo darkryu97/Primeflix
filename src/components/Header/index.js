21bb29c693b05743c8fa93976eebcce60700a6a9
DarkRyu97 <edu.obgemail@gmail.com>
2023-03-07 16:08:02 -0300

    Final Commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import './header.css'
+import { Link } from 'react-router-dom';
+
+function Header(){
+    return(
+        <header>
+            <h1>alo</h1>
+            <Link className="logo" to="/" >Prime Flix</Link>
+            <Link className="favoritos" to="/favoritos">Meus Filmes</Link>
+            
+        </header>
+    )
+}
+
+export default Header;
